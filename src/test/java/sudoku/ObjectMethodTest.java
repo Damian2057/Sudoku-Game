34120fb98a9580bf0a1fce94dec3e61eb37b95fb
236514 Dangreaux Mateusz <236514@edu.p.lodz.pl>
2021-11-19 18:37:57 +0100

    Wszystkie testy dla hashCode juz wykonane, rozpoczete testy toString

--- src/test/java/sudoku/ObjectMethodTest.java
+++ src/test/java/sudoku/ObjectMethodTest.java
@@ -1,6 +1,7 @@
 package sudoku;
 
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.ls.LSOutput;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -20,7 +21,6 @@ public class ObjectMethodTest {
         assertEquals(s1, s2);
         assertEquals(s1.hashCode(), s2.hashCode());
 
-        s2.getElement();
         s1.getElement();
         assertNotEquals(s1, s2);
         assertNotEquals(s1.hashCode(), s2.hashCode());
@@ -50,8 +50,50 @@ public class ObjectMethodTest {
         assertNotEquals(s1.hashCode(), s2.hashCode());
     }
 
+    @Test
+    public void hashCodeSudokuElementTest() {
+        SudokuField[] t = new SudokuField[9];
+        SudokuField[] t2 = new SudokuField[9];
+
+        for(int i = 0; i < 9; i++) {
+            t[i] = new SudokuField();
+            t2[i] = new SudokuField();
+            t[i].setFieldValue(i);
+            t2[i].setFieldValue(9-i);
+        }
+
+        SudokuElement row1 = new SudokuRow(t);
+        SudokuElement row2 = new SudokuRow(t);
+        SudokuElement row3 = new SudokuRow(t2);
+
+        assertEquals(row1, row1);
+        assertEquals(row1.hashCode(), row1.hashCode());
+        assertEquals(row1, row2);
+        assertEquals(row1.hashCode(), row2.hashCode());
+        assertEquals(row3, row3);
+        assertEquals(row3.hashCode(), row3.hashCode());
 
 
+        assertNotEquals(row1, row3);
+        assertNotEquals(row1.hashCode(), row3.hashCode());
 
+
+    }
+
+    @Test
+    public void toStringBacktrackingSudokuSolverTest() {
+        BacktrackingSudokuSolver b1 = new BacktrackingSudokuSolver();
+        BacktrackingSudokuSolver b2 = new BacktrackingSudokuSolver();
+        b2.getElement();
+
+        assertEquals(b1.toString(), "BacktrackingSudokuSolver[randList=[]]");
+        assertNotEquals(b1.toString(), b2.toString());
+    }
+
+//    @Test
+//    public void toStringSudokuFieldTest() {
+//        SudokuField s1 = new SudokuField();
+//        SudokuField s2 = new SudokuField();
+//    }
 
 }
