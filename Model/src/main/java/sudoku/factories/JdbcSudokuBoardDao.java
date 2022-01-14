5fbb787ea96ec649ac47fb2240df21368edb368f
236514 Dangreaux Mateusz <236514@edu.p.lodz.pl>
2022-01-14 18:50:21 +0100

    implementacja funkcjonalnosci read()

--- Model/src/main/java/sudoku/factories/JdbcSudokuBoardDao.java
+++ Model/src/main/java/sudoku/factories/JdbcSudokuBoardDao.java
@@ -1,6 +1,8 @@
 package sudoku.factories;
 
 import org.apache.derby.shared.common.error.DerbySQLIntegrityConstraintViolationException;
+import org.slf4j.LoggerFactory;
+import sudoku.BacktrackingSudokuSolver;
 import sudoku.SudokuBoard;
 
 import java.io.IOException;
@@ -8,6 +10,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Logger;
 
 public class JdbcSudokuBoardDao implements Dao<SudokuBoard>{
     private static String dataBaseUrl;
@@ -19,24 +22,15 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>{
     public JdbcSudokuBoardDao(String boardName, String dataBaseUrl) throws SQLException {
         this.boardName = boardName;
         this.dataBaseUrl = dataBaseUrl;
-        createConnection();
+        createDatabase();
     }
 
-    private static void createConnection() throws SQLException {
+    private static void createDatabase() throws SQLException {
         try
         {
+
             conn = DriverManager.getConnection(dataBaseUrl);
             stmt = conn.createStatement();
-//            String SQL = "CREATE TABLE SudokuBoards (id INT NOT NULL AS IDENTITY(start with 1, INCREMENT by 1) PRIMARY KEY,"
-//                    + "name VARCHAR(40) NOT NULL)";
-//            stmt.execute(SQL);
-//            SQL = "CREATE TABLE SudokuFields (id INT NOT NULL,"
-//                    +"col_index INT NOT NULL,"
-//                    + "row_index INT NOT NULL,"
-//                    + "value INT NOT NULL,"
-//                    + "PRIMARY KEY (id, col_index, row_index),"
-//                    + "FOREIGN KEY (id) REFERENCES SudokuBoards(id))";
-//            stmt.execute(SQL);
 
             String SQL = "CREATE TABLE SudokuBoards" +
                     "(" +
@@ -55,6 +49,7 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>{
                     + "FOREIGN KEY (id) REFERENCES SudokuBoards(id)"
                     + ")";
             stmt.execute(SQL);
+            conn.commit();
         }
         catch (DerbySQLIntegrityConstraintViolationException except)
         {
@@ -62,9 +57,51 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>{
         }
     }
 
+    private void createConnection() throws SQLException {
+        conn = DriverManager.getConnection(dataBaseUrl);
+        stmt = conn.createStatement();
+    }
+
+    private void disconnectConnection() throws SQLException {
+        conn.close();
+        stmt.close();
+    }
+
     @Override
     public SudokuBoard read() throws IOException, ClassNotFoundException {
-        return null;
+        SudokuBoard temp = new SudokuBoard(new BacktrackingSudokuSolver());
+        try {
+            conn.setAutoCommit(false);
+            int board_id = getBoardIdFromDataBase();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return temp;
+    }
+
+    private int getBoardIdFromDataBase() throws SQLException {
+        org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
+        String query = "SELECT MAX(sudoku_board_id) "
+                + "FROM SudokuBoards "
+                + "WHERE sudoku_board_name = ?;";
+
+        try (var getBoardIDSStatement = conn.prepareStatement(query)) {
+            getBoardIDSStatement.setString(1, boardName);
+
+            try (var resultSet = getBoardIDSStatement.executeQuery()) {
+                int boardId = 0;
+                if (resultSet.next()) {
+                    boardId = resultSet.getInt(1);
+                } else {
+                    //throw new WrongNameJdbcException();
+                }
+                logger.debug("{}'s id is {}", boardName, boardId);
+                return boardId;
+            }
+        } catch (Exception e) {
+            conn.rollback();
+            throw e;
+        }
     }
 
     @Override
