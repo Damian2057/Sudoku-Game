e43abeaa9a914277aecb237ba2941b870bfb3f58
236514 Dangreaux Mateusz <236514@edu.p.lodz.pl>
2022-01-14 19:07:17 +0100

    dodawanie loggerow

--- Model/src/main/java/sudoku/factories/JdbcSudokuBoardDao.java
+++ Model/src/main/java/sudoku/factories/JdbcSudokuBoardDao.java
@@ -4,12 +4,12 @@ import org.apache.derby.shared.common.error.DerbySQLIntegrityConstraintViolation
 import org.slf4j.LoggerFactory;
 import sudoku.BacktrackingSudokuSolver;
 import sudoku.SudokuBoard;
+import sudoku.exceptions.dao.DaoException;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class JdbcSudokuBoardDao implements Dao<SudokuBoard>{
@@ -25,10 +25,11 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>{
         createDatabase();
     }
 
-    private static void createDatabase() throws SQLException {
+    private void createDatabase() throws SQLException {
+        org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
         try
         {
-
+            logger.info("Database creation attempt");
             conn = DriverManager.getConnection(dataBaseUrl);
             stmt = conn.createStatement();
 
@@ -53,7 +54,11 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>{
         }
         catch (DerbySQLIntegrityConstraintViolationException except)
         {
-            except.printStackTrace();
+            logger.error("Database integer error");
+            //except.printStackTrace();
+        }
+        catch (SQLException e) {
+            logger.debug("Database already exists");
         }
     }
 
@@ -134,12 +139,104 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>{
     }
 
     @Override
-    public void write(SudokuBoard obj) throws IOException {
+    public void write(SudokuBoard obj) {
+        org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
+        logger.debug("Starting writing to the database");
 
+        try (Connection connection = DriverManager.getConnection(dataBaseUrl)) {
+            connection.setAutoCommit(false);
+            int boardId = createBoardEntryInDataBase();
+            saveFieldsInDatabase(obj, boardId);
+
+            logger.debug("Committing changes in database");
+            connection.commit();
+            logger.debug("The writing query was successful");
+
+        } catch (SQLDataException e) {
+            logger.error("SQL data error");
+            //throw new SyntaxJdbcDaoException();
+        } catch (SQLSyntaxErrorException e) {
+            logger.error("SQL syntax error");
+            //throw new SyntaxJdbcDaoException();
+        } catch (SQLException e) {
+            logger.error("Cant write {} to database", boardName);
+            //throw new WrittingJdbcDaoException();
+        }
+    }
+
+    private void saveFieldsInDatabase(SudokuBoard obj,
+                                      int boardId) throws SQLException {
+        try (var createFieldStatement =
+                     conn.prepareStatement("INSERT INTO SudokuFields VALUES(?, ?, ?, ?);")) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    createFieldStatement.setInt(1, boardId);
+                    createFieldStatement.setInt(2, i);
+                    createFieldStatement.setInt(3, j);
+                    createFieldStatement.setInt(4, obj.get(i, j));
+                    createFieldStatement.executeUpdate();
+                }
+            }
+        } catch (Exception e) {
+            conn.rollback();
+            throw e;
+        }
+    }
+
+    private int createBoardEntryInDataBase() throws SQLException {
+        org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
+        logger.debug("Creating {} Entry", boardName);
+        String query = "INSERT INTO SudokuBoards VALUES (NULL, ?);";
+
+        try (var createBoardStatement =
+                     conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            createBoardStatement.setString(1, boardName);
+            createBoardStatement.executeUpdate();
+
+            try (var resultSet = createBoardStatement.getGeneratedKeys()) {
+                int boardID;
+                if (resultSet.next()) {
+                    boardID = resultSet.getInt(1);
+
+                    logger.debug("Created {} Entry with id: {}", boardName, boardID);
+                    return boardID;
+                } else {
+                    //throw new WrittingJdbcDaoException();
+                    throw new DaoException();
+                }
+            }
+        } catch (Exception e) {
+            conn.rollback();
+            throw e;
+        }
+
+    }
+
+    public static List<String> getAllBoardsInDataBase(String dataBaseUrl) {
+        var logger = LoggerFactory.getLogger(JdbcSudokuBoardDao.class);
+        String query = "SELECT sudoku_board_name FROM SudokuBoards GROUP BY sudoku_board_name;";
+
+        try (var connection = DriverManager.getConnection(dataBaseUrl)) {
+            var savedBoards = new ArrayList<String>();
+            try (var statement = connection.createStatement()) {
+                try (var resultSet = statement.executeQuery(query)) {
+                    while (resultSet.next()) {
+                        savedBoards.add(resultSet.getString(1));
+                    }
+                    return savedBoards;
+                }
+            }
+        } catch (SQLException e) {
+            logger.error("Something goes wrong during Data base query");
+            //throw new JdbcDaoException();
+            throw new DaoException();
+        }
     }
 
     @Override
     public void close() throws Exception {
 
     }
+
+
 }
