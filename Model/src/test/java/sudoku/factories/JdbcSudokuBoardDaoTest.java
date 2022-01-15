b9ea412ac8258ee8f08a97deff2ee0c83a6152b1
236514 Dangreaux Mateusz <236514@edu.p.lodz.pl>
2022-01-15 16:04:42 +0100

    test 83%

--- Model/src/test/java/sudoku/factories/JdbcSudokuBoardDaoTest.java
+++ Model/src/test/java/sudoku/factories/JdbcSudokuBoardDaoTest.java
@@ -1,33 +1,46 @@
 package sudoku.factories;
 
 import org.junit.jupiter.api.Test;
+import sudoku.BacktrackingSudokuSolver;
+import sudoku.SudokuBoard;
+import sudoku.exceptions.jdbc.SyntaxJdbcDaoException;
+import sudoku.exceptions.jdbc.WrongNameJdbcException;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JdbcSudokuBoardDaoTest {
 
     @Test
-    void read() {
-        
+    void readTest() {
+        try {
+            SudokuBoard sudoku1 = new SudokuBoard(new BacktrackingSudokuSolver());
+            sudoku1.solveGame();
+            SudokuBoard sudoku2 = new SudokuBoard(new BacktrackingSudokuSolver());
+            var file = SudokuBoardDaoFactory.getJdbcDao("test", "jdbc:derby:testowa_baza;create=true");
+            assertThrows(SyntaxJdbcDaoException.class, ()->file.write(sudoku1));
+            sudoku2 = file.read();
+
+            assertNotEquals(sudoku1, sudoku2);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    void write() {
+    void supertestTest() {
+        try {
+            SudokuBoard sudoku1 = new SudokuBoard(new BacktrackingSudokuSolver());
+            sudoku1.solveGame();
+            SudokuBoard sudoku2 = new SudokuBoard(new BacktrackingSudokuSolver());
+            var file = SudokuBoardDaoFactory.getJdbcDao("test", "jdbc:derby:testowa_baza;create=true");
 
+            assertDoesNotThrow(()->file.getAllBoardsInDataBase());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Test
-    void boolToInt() {
-
-    }
-
-    @Test
-    void getAllBoardsInDataBase() {
-
-    }
-
-    @Test
-    void close() {
-
-    }
 }
